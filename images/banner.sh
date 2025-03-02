0738b9e62dc75c69493f23e8d65236db7d83ae4f
luisarg03 <luis.m.paz.03@gmail.com>
2025-03-02 16:47:06 +0000

    Update terminal image and enhance banner script for improved welcome message

--- images/banner.sh
+++ images/banner.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-texto=$(figlet Welcome !)
+# Generar el texto con figlet
+texto=$(figlet Welcome  !!)
 
-  logo='          .-~~~-.
-       .-(       )_   
-    .-(             )  
+# Definir el logo de una nube con "AWS" y un engranaje dentro
+logo='          .-~~~-.
+       .-( ⚙ ⚙ ⚙  )_   
+    .-(     AWS    )  
    (                 ) 
    |                 |
     `-.___....___..-'
 
+# Dividir el texto y el logo en líneas
 mapfile -t lineas_texto <<< "$texto"
 mapfile -t lineas_logo <<< "$logo"
 
+# Combinar línea por línea
 for i in "${!lineas_texto[@]}"; do
-  echo -e "\e[95m${lineas_texto[i]}    ${lineas_logo[i]:-}\e[0m"
+  # Añadir un espacio entre el texto y el logo
+  echo -e "\e[36m${lineas_texto[i]}  ${lineas_logo[i]:-}\e[0m"
 done
